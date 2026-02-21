66f9a2d05261a638ee141af79086978a270ce607
Tim Fothergill <timothy.fothergill@gmail.com>
2026-02-21 22:47:50 +0000

    COS-008 adds initial styling

--- test/controllers/BaseControllerSpec.scala
+++ test/controllers/BaseControllerSpec.scala
@@ -2,17 +2,17 @@ package controllers
 
 import org.scalatest._
 import org.scalatestplus.play._
-import org.scalatestplus.play.guice.GuiceOneAppPerTest
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 import play.api.{Play, Application}
 import play.api.inject.guice._
 
-class BaseControllerSpec extends PlaySpec with GuiceOneAppPerTest {
+class BaseControllerSpec extends PlaySpec with GuiceOneAppPerSuite {
     override def fakeApplication() = 
         new GuiceApplicationBuilder().configure(
-            "play.http.secret.key" -> "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef",
-            "db.default.url" -> "jdbc:postgresql://localhost:5432/test",
-            "db.default.username" -> "test",
-            "db.default.password" -> "testpass"
+            "play.http.secret.key"  -> "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef",
+            "db.default.url"        -> "jdbc:postgresql://localhost:5432/test",
+            "db.default.username"   -> "test",
+            "db.default.password"   -> "testpass"
         )
         .build()
 }
